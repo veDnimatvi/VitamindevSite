198f3aa13f63becb2875453c3855a4797c649a3c
thiennt <thiennt@hivetech.vn>
2022-06-30 14:54:12 +0700

    feat: update avatar and my skill

--- pages/components/MySkill.js
+++ pages/components/MySkill.js
@@ -4,18 +4,18 @@ import html from "../../public/img/html-5.png";
 import css from "../../public/img/css3.png";
 import js from "../../public/img/javascript-100.png";
 import sass from "../../public/img/sass-100.png";
-import bootstrap from "../../public/img/bootstrap-100.png";
 import mui from "../../public/img/material-ui-100.png";
-import tailwind from "../../public/img/tailwind.svg";
 import antd from "../../public/img/antd.png";
 import typescript from "../../public/img/typescript-100.png";
 import react from "../../public/img/react-100.png";
 import vue from "../../public/img/vue-100.png";
 import angular from "../../public/img/angularjs-100.png";
 import java from "../../public/img/java-100.png";
-import node from "../../public/img/nodejs-100.png";
+import nextjs from "../../public/img/nextjs-100.png";
 import graphql from "../../public/img/graphql-100.png";
 import figma from "../../public/img/figma-100.png";
+import spring from "../../public/img/spring-100.png";
+import mariadb from "../../public/img/mariadb-100.png";
 
 export default function MySkill() {
   return (
@@ -33,6 +33,10 @@ export default function MySkill() {
           <p>CSS3</p>
         </div>
         <div className="list-skill__item">
+          <Image src={sass} alt="sass" />
+          <p>Sass</p>
+        </div>
+        <div className="list-skill__item">
           <Image src={js} alt="js" />
           <p>Javascript</p>
         </div>
@@ -41,21 +45,10 @@ export default function MySkill() {
           <p>Typescript</p>
         </div>
         <div className="list-skill__item">
-          <Image src={sass} alt="sass" />
-          <p>Sass</p>
-        </div>
-        <div className="list-skill__item">
-          <Image src={bootstrap} alt="bootstrap" />
-          <p>bootstrap</p>
-        </div>
-        <div className="list-skill__item">
           <Image src={mui} alt="mui" />
           <p>Material UI</p>
         </div>
-        <div className="list-skill__item">
-          <Image src={tailwind} alt="tailwind" width={100} height={100} />
-          <p>TailwindCss</p>
-        </div>
+
         <div className="list-skill__item">
           <Image src={antd} alt="antd" width={100} height={100} />
           <p>Ant Design</p>
@@ -63,6 +56,10 @@ export default function MySkill() {
         <div className="list-skill__item">
           <Image src={react} alt="react" width={100} height={100} />
           <p>ReactJS</p>
+        </div>
+        <div className="list-skill__item">
+          <Image src={nextjs} alt="nextjs" width={100} height={100} />
+          <p>NextJS</p>
         </div>
         <div className="list-skill__item">
           <Image src={vue} alt="vue" width={100} height={100} />
@@ -77,8 +74,12 @@ export default function MySkill() {
           <p>Java</p>
         </div>
         <div className="list-skill__item">
-          <Image src={node} alt="node" width={100} height={100} />
-          <p>NodeJs</p>
+          <Image src={spring} alt="spring" width={100} height={100} />
+          <p>Spring Boot</p>
+        </div>
+        <div className="list-skill__item">
+          <Image src={mariadb} alt="mariadb" width={100} height={100} />
+          <p>MariaDB</p>
         </div>
         <div className="list-skill__item">
           <Image src={graphql} alt="graphql" width={100} height={100} />
