dacf7dadc4151bc5302c9f3e45be435754ef6b28
thiennt <thiennt@hivetech.vn>
2022-07-01 17:54:52 +0700

    feat: new profile

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
---
+++ pages/my-profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MyProfile() {
+  return (
+    <div className="my-profile">
+      <h1 class="my-name">VITAMINDEV</h1>
+    </div>
+  );
+}
