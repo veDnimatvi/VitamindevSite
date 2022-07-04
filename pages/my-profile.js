9e3a375fcc0b3d274dfaac2518d8d85bf306aca9
thiennt <thiennt@hivetech.vn>
2022-07-04 17:26:20 +0700

    feat: new header and about

--- pages/my-profile.js
+++ pages/my-profile.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from "react";
 import logo from "../public/img/my-logo.svg";
 import menuIcon from "../public/img/icons-menu.svg";
 import img from "../public/img/new-avatar.jpg";
+import face from "../public/img/facebook-100.png";
+import github from "../public/img/github-100.png";
+import linkedin from "../public/img/linkedin-100.png";
+import twitter from "../public/img/twitter-100.png";
 
 export default function MyProfile() {
   const [openMenuMobile, setOpenMenuMobile] = useState(false);
@@ -72,8 +76,8 @@ export default function MyProfile() {
         <div className="text-xl cursor-pointer mb-12 ml-16">Contact</div>
       </div>
 
-      <div className="introduce container mx-auto">
-        <div className="avatar-group flex justify-center">
+      <div className="introduce">
+        <div className="flex justify-center">
           <div className="my-avatar">
             <Image
               className="logo2"
@@ -83,21 +87,79 @@ export default function MyProfile() {
               height={150}
             />
             <div className="text">
-              <p className="font-bold text-black">
+              <p className="font-bold text-white">
                 Nguyễn Thanh Thiên - Web Developer -{" "}
               </p>
             </div>
           </div>
         </div>
 
-        <div className="introduce__title">
-          <div className="font-bold text-xl">Hello !</div>
-          <div className="md:text-2xl lg:text-6xl">
-            I am <span className="text-orange-300">Thiên</span>
+        <div className="text-6xl text-center text-white">
+          I am <span className="text-orange-300">Thiên</span>
+        </div>
+        <div className="text-6xl text-center text-white">
+          I am <span className="text-orange-300">a web developer</span> <br />
+          in Hanoi !
+        </div>
+      </div>
+
+      <div className="container mx-auto">
+        <div className="about">
+          <div className="about__title">
+            <div className="text-3xl">About me</div>
+            <div className="w-40 h-1 bg-orange-300 rounded"></div>
+            <div>I want to be a web developer and web designer</div>
+            <div>My hobbies are playing sports, playing music, traveling </div>
           </div>
-          <div className="md:text-2xl lg:text-6xl">
-            I am <span className="text-orange-300">a web developer</span> <br />
-            in Hanoi !
+          <div className="about__info pt-10">
+            <div>Name: Nguyễn Thanh Thiên</div>
+            <div>Born : August 09, 1999</div>
+            <div>Email : codertom100@gmail.com</div>
+            <div>Phone : 0345000921</div>
+
+            <div className="wrapper">
+              <div
+                className="icon facebook"
+                onClick={() =>
+                  window.open("https://www.facebook.com/nt.thien.9899/")
+                }
+              >
+                <div className="tooltip">Facebook</div>
+                <span>
+                  <Image className="logo2" src={face} alt="icon face" />
+                </span>
+              </div>
+              <div
+                className="icon github"
+                onClick={() => window.open("https://github.com/vitamindevplus")}
+              >
+                <div className="tooltip">GitHub</div>
+                <span>
+                  <Image className="logo2" src={github} alt="icon github" />
+                </span>
+              </div>
+              <div
+                className="icon twitter"
+                onClick={() => window.open("https://twitter.com/vitamindev100")}
+              >
+                <div className="tooltip">Twitter</div>
+                <span>
+                  <Image className="logo2" src={twitter} alt="icon twitter" />
+                </span>
+              </div>
+
+              <div
+                className="icon linkedin"
+                onClick={() =>
+                  window.open("https://www.linkedin.com/in/vitamindev/")
+                }
+              >
+                <div className="tooltip">Linkedin</div>
+                <span>
+                  <Image className="logo2" src={linkedin} alt="icon linkedin" />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
