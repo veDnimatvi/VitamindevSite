8f243fad5a735249c1ad54dcb997cba5c6f443b8
thiennt <thiennt@hivetech.vn>
2022-07-15 14:35:26 +0700

    feat: update title and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Vitamindev</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
