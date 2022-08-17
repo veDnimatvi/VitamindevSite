e7a19757c594b33f97b6abfa57cf2b84d9c40970
thiennt <thiennt@hivetech.vn>
2022-08-17 16:33:23 +0700

    feat: change router

--- pages/components/PostItem.jsx
+++ pages/components/PostItem.jsx
@@ -11,7 +11,7 @@ const PostItem = ({ listItems, typeOfPost }) => {
           key={i}
           onClick={() =>
             router.push(
-              `/${typeOfPost === "fe" ? "vitaminsfe" : "vitaminsbe"}/${
+              `${typeOfPost === "fe" ? "vitaminsfe" : "vitaminsbe"}/${
                 blog?.slug
               }`
             )
