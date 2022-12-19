463195d25f7a1a497bb3a917c944ffa78baa6405
thiennt <thiennt@hivetech.vn>
2022-12-19 14:40:19 +0700

    fix: send email function

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,48 +1,54 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import sendMessageImg from "../public/img/contact-me.svg";
-import axios from "axios";
+// import axios from "axios";
+import emailjs from "@emailjs/browser";
 
 const CustomizeAlert = ({ messge }) => {
   return <div className="p-2 mb-4 bg-green-300 rounded">{messge}</div>;
 };
 
 const Contact = () => {
-  const [message, setMessage] = useState("");
-  const [email, setEmail] = useState("");
+  const form = useRef();
+  // const [message, setMessage] = useState("");
+  // const [email, setEmail] = useState("");
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
   const [messageAlert, setMessageAlert] = useState("");
 
-  function isValidEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-  }
+  // function isValidEmail(email) {
+  //   return /\S+@\S+\.\S+/.test(email);
+  // }
 
-  const sendEmail = () => {
-    if (!isValidEmail(email) || message === "") {
-      setError(true);
-    } else {
-      setError(false);
-      setLoading(true);
-      axios
-        .post("https://send-email-contact-me.herokuapp.com/send", {
-          yourEmail: email,
-          subject: "My email is " + email,
-          message,
-        })
-        .then((res) => {
-          setMessageAlert(res?.data);
-          setEmail("");
-          setMessage("");
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    // setLoading(true);
+
+    emailjs
+      .sendForm(
+        "service_3x3yoer",
+        "template_16o4827",
+        form.current,
+        "x5Pm5_4b8Cj91CbYb"
+      )
+      .then(
+        (result) => {
+          setMessageAlert("Send Successful!");
+          // setEmail("");
+          // setMessage("");
           setSuccess(true);
-          setLoading(false);
+          // setLoading(false);
           setTimeout(() => setSuccess(false), 3000);
-        })
-        .catch((err) => console.log("err", err));
-    }
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
@@ -58,25 +64,30 @@ const Contact = () => {
             <Image src={sendMessageImg} alt="send message img" />
           </div>
           <div className="flex justify-center">
-            <div className="send-me__form">
+            <form ref={form} onSubmit={sendEmail} className="w-full">
               <div className="send-me__form__input">
                 <input
-                  className="text-black"
                   type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Your email ..."
+                  name="user_name"
+                  className="form-control"
+                  placeholder="Your Name ..."
+                />
+              </div>
+              <div className="send-me__form__input">
+                <input
+                  type="email"
+                  name="user_email"
+                  className="form-control"
+                  placeholder="Your Email ..."
                 />
               </div>
               <div className="send-me__form__input">
                 <textarea
-                  className="text-black"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
+                  name="message"
+                  className="form-control"
                   placeholder="Your message ..."
                 />
               </div>
-
               {error && (
                 <h2 className="mb-4 text-red-500">
                   Email phải hợp lệ và message không được để trống !
@@ -84,22 +95,12 @@ const Contact = () => {
               )}
 
               {success && <CustomizeAlert messge={messageAlert} />}
-
-              <button
-                onClick={sendEmail}
-                className="flex items-center p-2 text-white bg-orange-400 rounded"
-              >
-                {loading && (
-                  <div className="lds-ring">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                )}
-                Send message
-              </button>
-            </div>
+              <input
+                type="submit"
+                value="Send Message"
+                className="p-2 text-white bg-orange-400 rounded cursor-pointer"
+              />
+            </form>
           </div>
         </div>
       </div>
