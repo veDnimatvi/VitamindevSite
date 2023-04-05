e4b65d4b72079b6bd4cb211a38f9638182f3ac98
thiennt <thiennt@hivetech.vn>
2023-04-05 17:21:02 +0700

    add: language attr

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     domains: ["www.monterail.com"],
     // domains: ["localhost"],
   },
+  i18n: {
+    locales: ["vi"],
+    defaultLocale: "vi",
+  },
 };
 
 module.exports = nextConfig;
