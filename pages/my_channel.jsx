ad4c1c9941d0cdc0a2dcc1bdc8ed2fbd58bb66bc
codeninzza <codertom100@gmail.com>
2024-03-04 09:50:44 +0700

    feat: add new youtube

--- pages/my_channel.jsx
+++ pages/my_channel.jsx
@@ -134,6 +134,68 @@ const MyChanel = () => {
               </div>
             </div>
           </div>
+
+          <div className="sm:flex p-8 gap-10 my-10 cursor-pointer shadow-[rgba(0, 0, 0, 0.35)] shadow-xl">
+            <iframe
+              className="sm:w-[560px] w-[300px]"
+              height="315"
+              src="https://www.youtube.com/embed/t4FG_6Ffmhw?si=R6RxCk6pYIRoc7rW"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullscreen
+            ></iframe>
+            <div>
+              <p className="my-4 text-xl font-semibold sm:mb-4 sm:mt-0">
+                Dự án tên Axios. Là một sản phẩm e-commerce sử dụng VueJs,
+                NuxtJs, NestJs, MySql, Sequelize. Lưu trữ hình ảnh bằng
+                FireBase.
+              </p>
+              <div className="text-sm">
+                <p>
+                  Được xây dựng dựa trên framework Nestjs cho backend, Nuxtjs và
+                  Tailwindcss cho phần website khách hàng, Vuejs và ElementUI
+                  cho phần website quản trị.
+                </p>
+                <p>&#10155; Phía Client</p>
+                <ul className="pl-5 list-disc">
+                  <li>
+                    Đăng ký, đăng nhập, đăng nhập bằng google, quên mật khẩu
+                    (reset mật khẩu).
+                  </li>
+                  <li>Gửi email liên hệ.</li>
+                  <li>
+                    Giỏ hàng, Mua hàng, Thanh toán online vnpay, Thanh toán COD,
+                    Lịch sử mua hàng.
+                  </li>
+                  <li>
+                    Tìm kiếm sản phẩm, Xem chi tiết sản phẩm, Bình luận, Đánh
+                    giá sản phẩm.
+                  </li>
+                </ul>
+                <p>&#10155; Phía Admin</p>
+                <ul className="pl-5 list-disc">
+                  <li>Thống kê doanh thu.</li>
+                  <li>
+                    Quản lý danh mục, sản phẩm, phân loại hàng, tài khoản , đơn
+                    hàng, đánh giá..
+                  </li>
+                  <li>Thay đổi mật khẩu , tên user, avatar.</li>
+                  <li>Phân quyền cho user, staff, admin.</li>
+                </ul>
+
+                <div
+                  className="flex items-center gap-2 p-3 mt-4 text-white duration-300 ease-in-out rounded bg-stone-800 w-max hover:scale-110"
+                  onClick={() =>
+                    window.open("https://www.youtube.com/watch?v=t4FG_6Ffmhw")
+                  }
+                >
+                  <Image src={youtube} alt="youtube" />
+                  <div>Xem Trên Youtube</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
