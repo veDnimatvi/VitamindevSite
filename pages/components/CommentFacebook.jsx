48b483f1db0578dbec7143eba0069656cc81e94c
codeninzza <codertom100@gmail.com>
2024-04-23 12:03:44 +0700

    feat: like post

--- pages/components/CommentFacebook.jsx
+++ pages/components/CommentFacebook.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { initFacebookSDK } from "../../utils/blogFunction";
+import { useRouter } from "next/router";
 
 const CommentsFacebook = () => {
+  const router = useRouter();
+
   useEffect(() => {
     initFacebookSDK();
   }, []);
@@ -11,10 +14,21 @@ const CommentsFacebook = () => {
       <div id="fb-root"></div>
       <div
         className="fb-comments"
-        data-href="https://websitecuathien.vercel.app/"
+        data-href={`https://websitecuathien.vercel.app${router?.asPath}`}
         data-numposts="10"
         data-width="100%"
       ></div>
+      <div style={{ padding: "0 5px" }}>
+        <div
+          className="fb-like"
+          data-href={`https://websitecuathien.vercel.app${router?.asPath}`}
+          data-width=""
+          data-layout=""
+          data-action=""
+          data-size=""
+          data-share="true"
+        ></div>
+      </div>
     </React.Fragment>
   );
 };
