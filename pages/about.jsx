7f320f25b0c9a001e9106cb969b10a249f1ce068
codeninzza <codertom100@gmail.com>
2024-04-23 14:48:27 +0700

    feat: add more seo

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import img from "../public/img/avatar.png";
+import Head from "next/head";
 
 const About = () => {
   const listSkills = [
@@ -83,71 +84,79 @@ const About = () => {
   }, []);
 
   return (
-    <div className="flex flex-col justify-between h-screen">
-      <Header />
-      <div className="container mx-auto" data-aos="fade-up">
-        <div className="mx-10 mb-20">
-          <div className="text-4xl font-bold">About me</div>
-          <div className="w-40 h-1 bg-orange-300 rounded"></div>
-        </div>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>About Vitamindev</title>
+        <meta name="About Vitamindev" content="About Vitamindev"></meta>
+      </Head>
+      <div className="flex flex-col justify-between h-screen">
+        <Header />
+        <div className="container mx-auto" data-aos="fade-up">
+          <div className="mx-10 mb-20">
+            <div className="text-4xl font-bold">About me</div>
+            <div className="w-40 h-1 bg-orange-300 rounded"></div>
+          </div>
 
-        <div className="mx-10 md:flex">
-          <div className="flex flex-col justify-center md:mx-5">
-            <div className="flex justify-center">
-              <div className="my-avatar">
-                <Image
-                  className="logo2"
-                  src={img}
-                  alt="a"
-                  width={150}
-                  height={150}
-                />
-                <div className="text">
-                  <p className="font-bold">
-                    Nguyễn Thanh Thiên - Web Developer -{" "}
-                  </p>
+          <div className="mx-10 md:flex">
+            <div className="flex flex-col justify-center md:mx-5">
+              <div className="flex justify-center">
+                <div className="my-avatar">
+                  <Image
+                    className="logo2"
+                    src={img}
+                    alt="a"
+                    width={150}
+                    height={150}
+                  />
+                  <div className="text">
+                    <p className="font-bold">
+                      Nguyễn Thanh Thiên - Web Developer -{" "}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="md:mx-32">
-            <p className="mb-6 text-lg">
-              Tôi là Thiên. Hiện đang sinh sống và làm
-              việc tại Hà nội. Đây là nơi tôi lưu trữ cũng như chia sẻ những kiến thức lập trình mình
-              biết. Vì tôi biết cách tiếp thu kiến thức tốt nhất
-              là chia sẻ. Tôi đã có 3 năm kinh nghiệm làm việc tại ví trí lập trình viên front-end
-              với đa dạng framework ReactJS, Angular và
-              VueJS. Có kiến thức về lập trình về back-end với NodeJs và thường xuyên làm việc với NestJs framework.
-            </p>
+            <div className="md:mx-32">
+              <p className="mb-6 text-lg">
+                Tôi là Thiên. Hiện đang sinh sống và làm việc tại Hà nội. Đây là
+                nơi tôi lưu trữ cũng như chia sẻ những kiến thức lập trình mình
+                biết. Vì tôi biết cách tiếp thu kiến thức tốt nhất là chia sẻ.
+                Tôi đã có 3 năm kinh nghiệm làm việc tại ví trí lập trình viên
+                front-end với đa dạng framework ReactJS, Angular và VueJS. Có
+                kiến thức về lập trình về back-end với NodeJs và thường xuyên
+                làm việc với NestJs framework.
+              </p>
 
-            <a
-              href="https://drive.google.com/file/d/1SYgDuz8MTNUVHvIKwBsqh7VxfmHUhuXJ/view?usp=sharing"
-              target="_blank"
-              className="mb-6 text-lg text-blue-500 underline"
-              rel="noreferrer"
-            >
-              Download MyCV
-            </a>
-            <div className="mb-3 text-xl">My Skills</div>
-            <div className="flex flex-wrap">
-              {listSkills &&
-                listSkills?.map((item, index) => {
-                  return (
-                    <span
-                      key={index}
-                      className="px-2 py-1 mb-2 mr-2 text-xs font-medium text-white rounded-sm"
-                      style={{ background: item.bg }}
-                    >
-                      {item.name}
-                    </span>
-                  );
-                })}
+              <a
+                href="https://drive.google.com/file/d/1SYgDuz8MTNUVHvIKwBsqh7VxfmHUhuXJ/view?usp=sharing"
+                target="_blank"
+                className="mb-6 text-lg text-blue-500 underline"
+                rel="noreferrer"
+              >
+                Download MyCV
+              </a>
+              <div className="mb-3 text-xl">My Skills</div>
+              <div className="flex flex-wrap">
+                {listSkills &&
+                  listSkills?.map((item, index) => {
+                    return (
+                      <span
+                        key={index}
+                        className="px-2 py-1 mb-2 mr-2 text-xs font-medium text-white rounded-sm"
+                        style={{ background: item.bg }}
+                      >
+                        {item.name}
+                      </span>
+                    );
+                  })}
+              </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import sendMessageImg from "../public/img/contact-me.svg";
 // import axios from "axios";
 import emailjs from "@emailjs/browser";
+import Head from "next/head";
 
 const CustomizeAlert = ({ messge }) => {
   return <div className="p-2 mb-4 bg-green-300 rounded">{messge}</div>;
@@ -52,60 +53,70 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex flex-col justify-between h-screen">
-      <Header />
-      <div className="container mx-auto section" data-aos="fade-up">
-        <div className="send-me">
-          <div className="mb-20">
-            <div className="text-4xl font-bold">Contact me</div>
-            <div className="w-40 h-1 bg-orange-300 rounded"></div>
-          </div>
-          <div className="flex justify-center send-me__img">
-            <Image src={sendMessageImg} alt="send message img" />
-          </div>
-          <div className="flex justify-center">
-            <form ref={form} onSubmit={sendEmail} className="w-full">
-              <div className="send-me__form__input">
-                <input
-                  type="text"
-                  name="user_name"
-                  className="form-control"
-                  placeholder="Your Name ..."
-                />
-              </div>
-              <div className="send-me__form__input">
-                <input
-                  type="email"
-                  name="user_email"
-                  className="form-control"
-                  placeholder="Your Email ..."
-                />
-              </div>
-              <div className="send-me__form__input">
-                <textarea
-                  name="message"
-                  className="form-control"
-                  placeholder="Your message ..."
-                />
-              </div>
-              {error && (
-                <h2 className="mb-4 text-red-500">
-                  Email phải hợp lệ và message không được để trống !
-                </h2>
-              )}
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Contact with Vitamindev</title>
+        <meta
+          name="Contact with Vitamindev"
+          content="Contact with Vitamindev"
+        ></meta>
+      </Head>
+      <div className="flex flex-col justify-between h-screen">
+        <Header />
+        <div className="container mx-auto section" data-aos="fade-up">
+          <div className="send-me">
+            <div className="mb-20">
+              <div className="text-4xl font-bold">Contact me</div>
+              <div className="w-40 h-1 bg-orange-300 rounded"></div>
+            </div>
+            <div className="flex justify-center send-me__img">
+              <Image src={sendMessageImg} alt="send message img" />
+            </div>
+            <div className="flex justify-center">
+              <form ref={form} onSubmit={sendEmail} className="w-full">
+                <div className="send-me__form__input">
+                  <input
+                    type="text"
+                    name="user_name"
+                    className="form-control"
+                    placeholder="Your Name ..."
+                  />
+                </div>
+                <div className="send-me__form__input">
+                  <input
+                    type="email"
+                    name="user_email"
+                    className="form-control"
+                    placeholder="Your Email ..."
+                  />
+                </div>
+                <div className="send-me__form__input">
+                  <textarea
+                    name="message"
+                    className="form-control"
+                    placeholder="Your message ..."
+                  />
+                </div>
+                {error && (
+                  <h2 className="mb-4 text-red-500">
+                    Email phải hợp lệ và message không được để trống !
+                  </h2>
+                )}
 
-              {success && <CustomizeAlert messge={messageAlert} />}
-              <input
-                type="submit"
-                value="Send Message"
-                className="p-2 text-white bg-orange-400 rounded cursor-pointer"
-              />
-            </form>
+                {success && <CustomizeAlert messge={messageAlert} />}
+                <input
+                  type="submit"
+                  value="Send Message"
+                  className="p-2 text-white bg-orange-400 rounded cursor-pointer"
+                />
+              </form>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
